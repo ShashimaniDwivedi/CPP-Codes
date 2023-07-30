3ae3f87c3a4957fedc211c8b9eab7e93d89ed93f
ShashimaniDwivedi <shashimanidwivedi6@gmail.com>
2023-07-30 11:00:44 +0530

    Fibonacci-Series

---
+++ Fibonacci.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  int i = 0;
+  int n1 = 0, n2 = 1, n3;
+  cout << "Enter a number : " << endl;
+  cin >> n;
+  cout << "Fibonacci series : ";
+  cout << n1 << " " << n2 << " ";
+  for (int i = 2; i < n; i++)
+  {
+    n3 = n1 + n2;
+    cout << n3 << " ";
+    n1 = n2;
+    n2 = n3;
+  }
+
+  return 0;
+}
